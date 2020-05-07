47be1ff5a29f2bdbd2912f483e3fd8ea86452ed5
Ahmad Hammouda <ahmedvb@hotmail.com>
2020-05-07 12:46:42 +0200

    save202005071246

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -5,6 +5,8 @@ export default function Download() {
     const arr = [1, 5, 8, 20, 9, 56];
     const [...a] = arr;
     const [buttonVariant, setButtonVariant] = useState(true);
+    const jobs =['doctor','teacher','engineer'];
+    const jobs2=['google',...jobs,'hotmail','microsoft'];
     function updateTime() {
         alert(a);
     }
@@ -14,5 +16,8 @@ export default function Download() {
     return <div>
         <h1>{time}</h1>
         <button className={buttonVariant?"btn btn-danger btn-lg":"btn btn-primary btn-lg"} onClick={UpdateButtonVariant}>Update</button>
+        <div>{jobs2.map(item=>{
+            return <h6>{item}</h6>
+        })}</div>
     </div>;
 }
