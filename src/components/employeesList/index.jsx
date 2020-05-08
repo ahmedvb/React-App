cc45596562e205c9e403ede2ea22b836bc9a4d22
Ahmad Hammouda <ahmedvb@hotmail.com>
2020-05-08 20:41:13 +0200

    save202005082041

--- src/components/employeesList/index.jsx
+++ src/components/employeesList/index.jsx
@@ -3,13 +3,17 @@ import Employee from "../employee";
 export default function EmployeeList(props) {
     const employees = [{ name: "Ahmad", job: "Developer" }, { name: "Gamal", job: "Plummer" }];
     const [isDone, setIsDone] = useState(false);
-    function deleteEmployee() {
-        setIsDone(prev => { return !prev; })
+    const [items, setItems] = useState([]);
+    function deleteEmployee(id) {
+        setItems(prev => {
+            return prev.filter((item, index) => { return id !== index; });
+        });
+        alert(id);
     }
     return (
         <table className="table table-responsive">
-            <tbody className={isDone ? "table-info" : "table-primary"}>
-                {employees.map((emp, index) => <Employee name={emp.name} job={emp.job} key={index} todelete={deleteEmployee} />)}
+            <tbody>
+                {employees.map((emp, index) => <Employee name={emp.name} job={emp.job} todelete={deleteEmployee} key={index} id={index} />)}
             </tbody>
         </table>
     );
